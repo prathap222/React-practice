104c4ccecbb3cafab87e808b284d8d8b3486863e
Prathap <prathap.sims@gmail.com>
2022-11-28 21:39:13 -0800

    Form validation

--- src/App.js
+++ src/App.js
@@ -1,34 +1,132 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+import {useFormik} from 'formik';
+import * as yup from "yup"
 import './App.css';
-import Navbar from './components/Navbar';
-import TodoList from './components/TodoList';
-import ThemeContextProvider from './contexts/ThemeContext';
-import AuthContextProvider from './contexts/AuthContext';
-import One from './components/One';
+import { min, values } from 'lodash';
 
-export const studentContext = React.createContext("");
+// Formik and Yup
+// npm install formik yup --save
 
+// const validate = values => {
+//   const errors = {};
+//   if(!values.name){
+//     errors.name = "Name is required"
+//   } else if (values.name.length >15){
+//     errors.name = "Maximum 15 charecters only"
+//   } else if (values.name.length < 5){
+//     errors.name = "Minimum 5 charecters required"
+//   }
+//   return errors;
+// }
 
-function App() {
-  return (
-    <div className='App'>
+const App = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      list:"",
+      password:"",
+      confirmpassword:""
+    },
+    validationSchema:yup.object({
+      name:yup.string()
+      .required("Name is required")
+      .strict()
+      .trim()
+      .min(5 , "Minimum 5 characters required")
+      .max(15 ,"Maximum 15 charecters only" ),
+      email:yup.string()
+      .email()
+      .required("Email is required"),
+      list:yup.string()
+      .required("List is required"),
+      password:yup.string()
+      .required("Password is required"),
+      confirmpassword:yup.string()
+      .oneOf([yup.ref('password'), null], "Confirm password and  password must be same")
+      .required("Confirmpassword is required")
       
-   <div className='ui raised very padded text container segment' >
-    <AuthContextProvider>
-    <ThemeContextProvider>
-    <Navbar/>
-    <TodoList/>
- 
-    
       
-      <One/>
-   
+    }),
     
-    </ThemeContextProvider>
-    </AuthContextProvider>
-   </div>
-   </div>
-  );
+    onSubmit:(userInputData) => {
+      console.log(userInputData);
+    }
+  })
+  return(
+    <div className='container mt-3'> 
+    <div className='jumbotron'>
+      <form autoComplete='off' onSubmit={formik.handleSubmit}>
+        <div className='form-group'>
+        <label>Name:</label>
+        <input 
+        className='form-control'
+        type="text" 
+        name='name' 
+        onChange={formik.handleChange} 
+        value = {formik.values.name}
+        />
+        {formik.errors.name ? <div className='text-danger'>{formik.errors.name}</div> : null}
+        </div>
+
+        <div className='form-group'>
+        <label>Email:</label>
+        <input 
+        className='form-control'
+        type="text" 
+        name='email' 
+        onChange={formik.handleChange} 
+        value = {formik.values.email}
+        />
+        {formik.errors.email ? <div className='text-danger'>{formik.errors.email}</div> : null}
+        </div>
+        <div className="form-group">
+  <label >Select list:</label>
+  <select
+  name='list' 
+  onChange={formik.handleChange} 
+  value = {formik.values.list}
+  
+  className="form-control" id="sel1">
+    <option value="">-- Select one --</option>
+    <option value="1">MBA</option>
+    <option value="2">BCA</option>
+    <option value="3">GED</option>
+  </select>
+  {formik.errors.list ? <div className='text-danger'>{formik.errors.list}</div> : null}
+</div>
+
+<div className='form-group'>
+        <label>Password:</label>
+        <input 
+        className='form-control'
+        type="text" 
+        name='password' 
+        onChange={formik.handleChange} 
+        value = {formik.values.password}
+        />
+        {formik.errors.password ? <div className='text-danger'>{formik.errors.password}</div> : null}
+        </div>
+
+        <div className='form-group'>
+        <label>Confirm password:</label>
+        <input 
+        className='form-control'
+        type="text" 
+        name='confirmpassword' 
+        onChange={formik.handleChange} 
+        value = {formik.values.confirmpassword}
+        />
+        {formik.errors.confirmpassword ? <div className='text-danger'>{formik.errors.confirmpassword}</div> : null}
+        </div>
+
+        <button className='btn btn-primary'>Submit</button>
+      </form>
+      </div>
+      </div>
+  )
 }
+
 
 export default App;
--- src/components/ClassStudents.jsx
+++ src/components/ClassStudents.jsx
@@ -1,31 +1,38 @@
 import React, { useContext, useReducer } from 'react';
 import { studentContext } from '../App';
 
-const initialCount = 0;
-const reducer = (state, action) => {
-  switch(action){
-    case 'INCREMENT':
-      return state + 1
+// const initialCount = 0;
+// const reducer = (state, action) => {
+//   switch(action){
+//     case 'INCREMENT':
+//       return state + 1
 
-      case 'DECREMENT':
-        return state -1
+//       case 'DECREMENT':
+//         return state -1
 
-        default:
-          return state;
-  }
-}
+//         default:
+//           return state;
+//   }
+// }
 
 export const ClassStudents = () => {
 
-  const [state, dispatch] = useReducer(reducer, initialCount)
+  // const [state, dispatch] = useReducer(reducer, initialCount)
+  const students = useContext(studentContext);
 
   return (
     <div>
-      <h1>
+      {/* <h1>
        {state}
       </h1>
       <button onClick={()=> dispatch("INCREMENT") }>INCREMENT</button>
-      <button onClick={()=> dispatch("DECREMENT") }>DECREMENT</button>
+      <button onClick={()=> dispatch("DECREMENT") }>DECREMENT</button> */}
+
+      <ul>
+        {
+          students.map(e => <li key={e}>{e}</li>)
+        }
+      </ul>
       </div>
   )
 }
